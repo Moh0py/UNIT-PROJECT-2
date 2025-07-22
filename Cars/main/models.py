51724eb2d57976460dd912b7ffc4f9890422c8c4
Moh0py <1vipmoh@gmail.com>
2025-07-22 12:28:11 +0300

    pages and style & more idea

--- Cars/main/models.py
+++ Cars/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+
+    name = models.CharField(max_length=100)
+    modle = models.CharField(max_length=100)
+    price = models.IntegerField()
+    
+
+
+
+class Review(models.Model):
+       car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='reviews')
+       name = models.CharField(max_length=255)
+       comment = models.TextField()
+       created_at = models.DateTimeField(auto_now_add=True)
