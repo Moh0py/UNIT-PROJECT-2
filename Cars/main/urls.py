c2726d64888238bdcfd5f7a839fc546c847ce2b6
Moh0py <1vipmoh@gmail.com>
2025-07-24 00:21:30 +0300

    fixed header & style website

--- Cars/main/urls.py
+++ Cars/main/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home_view'),
-    path('Cars/<int:car_id>/', views.car_view, name='car_view'),
+    path('Cars/', views.car_list, name='car_list'),        
+    path('Cars/<int:car_id>/', views.car_list, name='car_list'),
     path('about/', views.about, name='about'),
+    path('car_details/<int:car_id>/', views.car_details, name='car_details'),
 ]
--- Cars/main/views.py
+++ Cars/main/views.py
@@ -8,12 +8,16 @@ def home_view(request):
     return render(request, 'pages/home.html', {'cars': cars})
 
 
-def car_view(request, car_id):
-    car = Car.objects.all(Car, pk=car_id)
-    return render(request, 'pages/car.html', {'car': car})
+def car_list(request):
+    cars = Car.objects.all()
+    return render(request, 'pages/car_list.html', {'cars': cars})
 
 def about(request):
     return render(request, 'pages/about.html')
 
 def contact(request):
     return render(request, 'contact/contact.html')
+
+def car_details(request,car_id):
+    car = Car.objects.all(Car, pk=car_id)
+    return render(request,'pages/deatils.html',{'car':car})
