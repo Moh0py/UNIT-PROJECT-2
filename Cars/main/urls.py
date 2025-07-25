dca992a84fa77e0f89e0c9d910dcedb8a0c624fd
Moh0py <1vipmoh@gmail.com>
2025-07-25 21:48:03 +0300

    compelet 3.0 used firebase & uplode images

--- Cars/main/urls.py
+++ Cars/main/urls.py
@@ -4,12 +4,14 @@ from contact import views as contact_views
 from django.urls import include
 
 app_name='cars'
+
 urlpatterns = [
     path('', views.home_view,name='home_view'),
     path('about/', views.about,name='about'),
     path('cars/', views.all_cars,name='all_cars'),
     path('cars/<int:car_id>/', views.car_detail, name='car_detail'),
-    path('cars/<int:car_id>/review/',views.add_review, name='add_review'),
     path('search/', views.search_car,name='search_car'),
     path('contact/', views.contact,name='contact'),
+    path('review/<int:car_id>/add/', views.add_review, name='add_review'),
+
 ]
--- Cars/main/views.py
+++ Cars/main/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.http import Http404
+from django.shortcuts import render, redirect
+from django.http import Http404, HttpRequest
 from django.core.paginator import Paginator
 from .models import Car, Review
 from .forms import CarForm, ReviewForm, CarFilterForm
@@ -28,9 +28,11 @@ def all_cars(request):
 
     return render(request, 'pages/all_cars.html', {'page_obj': page_obj})
 
-
 def car_detail(request, car_id):
-    car = get_object_or_404(Car, pk=car_id)  
+    car = Car.objects.filter(pk=car_id).first()
+    if not car:
+        raise Http404("Car not found")
+
     related_cars = Car.objects.filter(brand=car.brand).exclude(pk=car_id)[:4]
     reviews = car.reviews.all()
 
@@ -42,7 +44,6 @@ def car_detail(request, car_id):
     }
     return render(request, 'pages/car_detail.html', context)
 
-
 def about(request):
     return render(request, 'contact/about.html')
 
@@ -51,19 +52,13 @@ def contact(request):
 
 def search_car(request):
     query = request.GET.get('q', '')
-    if query:
-        results = Car.objects.filter(name__icontains=query)
-    else:
-        results = Car.objects.all()
-
+    results = Car.objects.filter(name__icontains=query) if query else Car.objects.all()
     return render(request, 'cars/search.html', {'results': results, 'query': query})
 
-
 def add_review(request, car_id):
-
- if request.method == 'POST':
-      car = Car.objects.get(pk=car_id)
-      review_text = Review(car=car, name=request.POST.get('name'), comment=request.POST.get('comment'))
-      review_text.save()
-      return redirect('car_detail', car_id=car.id)
-
+    if request.method == 'POST':
+     car = Car.objects.get(pk=car_id)
+    review_text = Review(car=car, name=request.POST.get('name'), comment=request.POST.get('comment'))
+    review_text.save()
+    return redirect('car_detail', car_id=car.id) 
+    
