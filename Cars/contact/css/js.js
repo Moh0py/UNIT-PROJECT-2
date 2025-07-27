b30cfa8fbbe24242e17bebdf41516fde1bd54b2e
Moh0py <1vipmoh@gmail.com>
2025-07-27 11:07:23 +0300

    compelet 3.0 used firebase & uplode images 2

---
+++ Cars/contact/css/js.js
@@ -0,0 +1,24 @@
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+    
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active');
+        }
+    }
+
+    
+    if (window.scrollY > 100) {
+        document.querySelector('.navbar').classList.add('scrolled');
+    } else {
+        document.querySelector('.navbar').classList.remove('scrolled');
+    }
+}
+
+window.addEventListener('load', reveal);
