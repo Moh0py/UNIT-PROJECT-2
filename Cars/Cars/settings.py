5129153eda6a2682cc3e30a4557a429dea797a0f
Moh0py <1vipmoh@gmail.com>
2025-07-30 23:59:06 +0300

    ready to deploy

--- Cars/Cars/settings.py
+++ Cars/Cars/settings.py
@@ -84,15 +84,6 @@ WSGI_APPLICATION = 'Cars.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
-    }
-} if not DEBUG else {
-    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
